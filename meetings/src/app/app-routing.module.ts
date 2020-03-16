4a4e68c09439707849f92c6c10812303b4a1c336
Silas Pedrosa <silas@implystudio.com>
2020-03-16 16:26:06 -0300

    setup

--- meetings/src/app/app-routing.module.ts
+++ meetings/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MeetingsListComponent } from "./meetings-list/meetings-list.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: MeetingsListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
