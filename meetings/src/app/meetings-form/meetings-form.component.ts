d280a2841cac6fc65640b8060933ab759b3097ec
Silas Pedrosa <silas@implystudio.com>
2020-03-17 02:13:16 -0300

    clean up

--- meetings/src/app/meetings-form/meetings-form.component.ts
+++ meetings/src/app/meetings-form/meetings-form.component.ts
@@ -15,19 +15,6 @@ export class MeetingsFormComponent implements OnInit {
   get subjects() {
     return this.form.get("subjects") as FormArray;
   }
-  // model: {
-  //   year: number;
-  //   month: number;
-  //   day: number;
-  //   description: string;
-  //   subjects: { description: string }[];
-  // } = {
-  //   year: new Date().getFullYear(),
-  //   month: new Date().getMonth() + 1,
-  //   day: new Date().getDate(),
-  //   description: "",
-  //   subjects: []
-  // };
   request: RemoteData<Meeting>;
   error: string;
 
@@ -54,18 +41,15 @@ export class MeetingsFormComponent implements OnInit {
         title: this.fb.control("")
       })
     );
-    console.log("subjects after push", this.subjects);
   }
 
   async submit() {
     this.error = null;
     this.request = new InProgress();
-    console.log(this.form.value);
     this.request = await this.meetingService.create(this.form.value);
     if (this.request instanceof Success) {
       this.modal.close(this.request);
     } else if (this.request instanceof Failure) {
-      console.log(this.request);
       this.error =
         this.request.error?.error?.errors[0] ||
         "Sorry, we couldn't process your action";
